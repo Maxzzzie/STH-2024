1c56cacbf9c9b577c88a24a69e5d3c360885a30d
Maxzzzie <maxboerboom@gmail.com>
2024-09-29 21:54:56 +0200

    added pma voice proximity voice chat plugin

--- .fxdk/map-resources/STHmap_SurviveTheHunt_Maxzzzie_fxworld/map.js
+++ .fxdk/map-resources/STHmap_SurviveTheHunt_Maxzzzie_fxworld/map.js
@@ -1,7 +1,7 @@
 // AUTOMATICALLY GENERATED FILE
 // ANY CHANGES TO THIS FILE WILL BE OVERWRITTEN
 //
-// Compiled at 2024-09-27T15.00.14
+// Compiled at 2024-09-29T21.29.41
 setTimeout(EnableEditorRuntime, 0);
 on('onResourceStop', (name) => { if (name === GetCurrentResourceName()) DisableEditorRuntime(); });
 // Map patches
--- MaxSTH/Client/MapIcons.cs
+++ MaxSTH/Client/MapIcons.cs
@@ -7,32 +7,6 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 
-// namespace STHMaxzzzie.Client
-// {
-
-//     public class MapIcons : BaseScript
-//     {
-//         //all icons should get deleted when it updates on the server.
-//         // Event handler for blip creation
-//         EventHandlers["blip:addBlip"] += new Action<Vector3, int, int, string>(OnAddBlip);
-//     }
-
-//     // This method is triggered when the server sends the 'blip:addBlip' event
-//     private void OnAddBlip(Vector3 position, int spriteId, int colorId, string blipName)
-//     {
-//         // Create the blip on the map
-//         Blip blip = AddBlipForCoord(position.X, position.Y, position.Z);
-//         SetBlipSprite(blip, spriteId); // Set the sprite (icon)
-//         SetBlipColour(blip, colorId); // Set the color
-//         SetBlipScale(blip, 1.0f); // Optionally set the size
-
-//         // Set the blip name (hover text)
-//         BeginTextCommandSetBlipName("STRING");
-//         AddTextComponentString(blipName);
-//         EndTextCommandSetBlipName(blip);
-
-//         // Optionally, make the blip short-range, so it only shows when the player is close
-//         SetBlipAsShortRange(blip, true);
-//     }
-// }
+namespace STHMaxzzzie.Client
+{}
 
